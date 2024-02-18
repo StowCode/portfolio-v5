80270035aa6beb1f359ead85eba2f4a0bdf29fde
Mitchell Stowman <mitchellstowman@gmail.com>
2024-02-17 20:11:37 -0500

    Fixed navbar organization. Issue with Tailwind

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,16 +1,11 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import Logo from '../public/images/StowcodeSolutions.svg';
+import Logo from '../../../public/images/StowcodeSolutions.svg';
+import LinkIcon from '../../../public/images/link-icon 1.svg';
 
 export default function Navbar() {
     return (
-        <nav 
-        className="m-5"
-        style={{ 
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'space-evenly'
-        }}>
+      <nav className="m-5 flex align-center justify-between">
         <div className="hamburger">
           <span></span>
           <span></span>
@@ -27,7 +22,15 @@ export default function Navbar() {
           />
         </div>
 
-        <div className="links"></div>
+        <div className="links">
+            <Image 
+                className="relative"
+                src={LinkIcon}
+                alt="Link Tree Icon"
+                width={30}
+                priority
+            />
+        </div>
       </nav>
     )   
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
-import Logo from '../../public/StowcodeSolutions.svg';
-import Navbar from '../../components/navbar';
+import Navbar from './components/navbar';
 
 export default function Home() {
   return (
