a2ee3deced149aae3d80fb209b75d0641f0e3cee
Mitchell Stowman <mitchellstowman@gmail.com>
2024-02-18 19:38:35 -0500

    Fixed issue with Navbar Styling

--- src/app/sections/navbar.tsx
+++ src/app/sections/navbar.tsx
@@ -5,31 +5,33 @@ import LinkIcon from '../../../public/images/link-icon 1.svg';
 
 export default function Navbar() {
     return (
-      <nav className="p-5 fixed w-full flex bg-[var(--clr-dark)] mx-auto align-center justify-between max-w-screen-xl">
-        <div className="hamburger flex flex-col hover:cursor-pointer">
-          <span className="block h-1 w-7 bg-white rounded-full mb-1"></span>
-          <span className="block h-1 w-5 bg-white rounded-full mb-1"></span>
-          <span className="block h-1 w-7 bg-white rounded-full"></span>
-        </div>
+      <nav className="p-5 fixed w-full bg-[var(--clr-dark)] mx-auto">
+        <div className="max-w-screen-xl mx-auto flex items-center justify-between">
+          <div className="hamburger flex flex-col hover:cursor-pointer">
+            <span className="block h-1 w-7 bg-white rounded-full mb-1"></span>
+            <span className="block h-1 w-5 bg-white rounded-full mb-1"></span>
+            <span className="block h-1 w-7 bg-white rounded-full"></span>
+          </div>
 
-        <div className="logo">
-          <Image 
-            className="relative"
-            src={Logo}
-            alt="Stowcode Solutions Logo"
-            width={250}
-            priority
-          />
-        </div>
-
-        <div className="links hover:cursor-pointer">
+          <div className="logo">
             <Image 
-                className="relative"
-                src={LinkIcon}
-                alt="Link Tree Icon"
-                width={30}
-                priority
+              className="relative"
+              src={Logo}
+              alt="Stowcode Solutions Logo"
+              width={250}
+              priority
             />
+          </div>
+
+          <div className="links hover:cursor-pointer">
+              <Image 
+                  className="relative"
+                  src={LinkIcon}
+                  alt="Link Tree Icon"
+                  width={30}
+                  priority
+              />
+          </div>
         </div>
       </nav>
     )   
