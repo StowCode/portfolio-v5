8b868e64334a238daae5aba6fe86c754c43e3cc9
Mitchell Stowman <mitchellstowman@gmail.com>
2024-03-18 19:05:35 -0400

    Escaped Characters Issue

--- app/sections/aboutme.tsx
+++ app/sections/aboutme.tsx
@@ -11,9 +11,15 @@ export default function AboutMe() {
                 </h2>
 
                 <article className="m-5 px-5 text-white font-light">
-                    <p className="my-4">I'm a Frontend Developer and UX designer, blending my background in visual design with a passion for intuitive and engaging user experiences. Formerly a music educator, I've transitioned into the world of software development, drawing upon my years of experience in delivering information effectively.</p>
-                    <p className="my-4">My journey has equipped me with the skills to create beautiful and effective products. Whether it's designing highly responsive websites that drive conversion rates or crafting immersive web experiences that captivate audiences, I thrive on the process of creation and building.</p>
-                    <p className="my-4">I pride myself on adaptability and overcoming challenges collaboratively. I'm eager to bring my expertise, ideas, and energy to your project, ensuring its success.</p>
+                    <div className="my-4">
+                        {"I'm a Frontend Developer and UX designer, blending my background in visual design with a passion for intuitive and engaging user experiences. Formerly a music educator, I've transitioned into the world of software development, drawing upon my years of experience in delivering information effectively."}
+                    </div>
+                    <div className="my-4">
+                        {"My journey has equipped me with the skills to create beautiful and effective products. Whether it's designing highly responsive websites that drive conversion rates or crafting immersive web experiences that captivate audiences, I thrive on the process of creation and building."}
+                    </div>
+                    <div className="my-4">
+                        {"I pride myself on adaptability and overcoming challenges collaboratively. I'm eager to bring my expertise, ideas, and energy to your project, ensuring its success."}
+                    </div>
                 </article>
                 
                 <a href="/Stowman-Resume.pdf" download>
