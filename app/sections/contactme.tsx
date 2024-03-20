edacc4f8dc7cf4679f5f8a1b44833ab9480f7ad3
Mitchell Stowman <mitchellstowman@gmail.com>
2024-03-20 09:17:41 -0400

    SVG adjustment

--- app/sections/contactme.tsx
+++ app/sections/contactme.tsx
@@ -8,16 +8,6 @@ export default function ContactMe() {
             <h2 className="ml-5 mr-20 text-left text-white text-3xl font-bold shrink-0">
                 Contact <span className="font-bold text-[var(--clr-accent)]">Me</span>
             </h2>
-
-{/*             <form action="https://formsubmit.co/6e5f5e258bfbdab8c20dd68c8904d89f" method="POST" className="flex flex-col sm:w-full w-11/12 max-w-screen-sm mx-5">
-                <input type="hidden" name="_template" value="table"></input>
-                <input type="hidden" name="_next" value="https://stowcode.tech"></input>
-                <input type="hidden" name="_captcha" value="false"></input>
-                <input type="Name" name="Name" className="mb-5 text-white bg-transparent border-b-2 border-[var(--clr-accent)] py-2" placeholder="Name"></input>
-                <input type="Email" name="Email" className="mb-5 text-white bg-transparent border-b-2 border-[var(--clr-accent)] py-2" placeholder="Email"></input>
-                <input type="Message" name="Message" className="bg-transparent pb-10 border-b-2 border-[var(--clr-accent)] text-white" placeholder="Message"></input>
-                <button className="w-32 font-bold text-[var(--clr-dark)] bg-[--clr-accent] rounded-sm mt-5 p-1 hover:cursor-pointer" type="submit">Send</button>
-            </form> */}
             
             <form action="https://submit-form.com/RClQlOhku" className="flex flex-col sm:w-full w-11/12 max-w-screen-sm mx-5">
                 <input type="text" id="name" name="name" placeholder="Name" required className="mb-5 text-white bg-transparent border-b-2 border-[var(--clr-accent)] py-2"/>
