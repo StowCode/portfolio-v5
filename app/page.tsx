32d6ea40553e8d14a3788cc6647923666c3560e1
Mitchell Stowman <mitchellstowman@gmail.com>
2024-05-01 14:15:17 -0400

    meta image test

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Navbar from './navbar/navbar';
 import Hero from './sections/hero';
 import WhatIDo from './sections/whatIdo';
@@ -5,10 +6,14 @@ import AboutMe from './sections/aboutme';
 import FeaturedWork from './sections/featuredwork';
 import ContactMe from './sections/contactme';
 import Footer from './sections/footer';
+import MetaImage from '../app/opengraph-image.jpg';
 
 export default function Home() {
   return (
     <main>
+      <Head>
+        <meta name="image" property="og:image" content="../app/opengraph-image.jpg" />
+      </Head>
       <Navbar />
       <Hero />
       <WhatIDo />
