87057f6d20a20c4def51be08aeaa2e0840d72af6
Mitchell Stowman <mitchellstowman@gmail.com>
2024-05-01 14:00:51 -0400

    OG Image

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Navbar from './navbar/navbar';
 import Hero from './sections/hero';
 import WhatIDo from './sections/whatIdo';
@@ -5,10 +6,15 @@ import AboutMe from './sections/aboutme';
 import FeaturedWork from './sections/featuredwork';
 import ContactMe from './sections/contactme';
 import Footer from './sections/footer';
+import MetaImage from '../public/images/ogimage.jpg';
 
 export default function Home() {
   return (
     <main>
+      <Head>
+        <meta property="og:image" content="../public/images/ogimage.jpg" />
+      </Head>
+
       <Navbar />
       <Hero />
       <WhatIDo />
